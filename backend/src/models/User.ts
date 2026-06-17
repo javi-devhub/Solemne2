f0548846e57e1e4b247c3d4f642fb8f198efdb78
Dedli-debug <doyarzoa3@correo.uss.cl>
2026-06-16 21:07:06 -0400

    Creacion de backend inicial con endpoints de registro y login (#21)
    * Creacion de backend inicial con endpoints de registro y login
    
    * Update ci.yml
    
    ---------
    
    Co-authored-by: Dedli-debug <doyarzo3@correo.uss.cl>

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,1 +1,27 @@
+import mongoose, { Schema } from 'mongoose';
 
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.model('User', userSchema);
